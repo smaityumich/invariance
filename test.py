695b4beeac72b47951e7892d92c13aef24ab86de
Subha Maity <smaity@smaity-mbp.local>
2020-04-08 20:24:32 -0400

    modified:   setup.py modified:   test.py

--- test.py
+++ test.py
@@ -61,7 +61,7 @@ data_test = [[x0, y0], [x1, y1]]
 
 
 reg_wasserstein, reg_var, lr, gamma_wasserstein, wasserstein_epoch = float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]), int(float(sys.argv[5]))
-num_steps = 10000
+num_steps = 100
 sinkhorn_iter = 5
 fitted_graph, current_time, expt_id = InvarLabelShift(data_train, data_test, num_steps=num_steps, 
                         reg_wasserstein=reg_wasserstein, reg_var = reg_var, learning_rate = lr, 
