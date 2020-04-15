2522322f2530a648e9bf77f1f5ba97c0551f8df2
smaityumich <56092728+smaityumich@users.noreply.github.com>
2020-04-15 11:06:06 -0400

    kjhsdkjfs

--- SLURM-Job-Submitter.py
+++ SLURM-Job-Submitter.py
@@ -7,13 +7,13 @@ import itertools
 job_file = 'submit.sbat'
 
 # Experiment 1
-reg_w = np.array(range(1, 20))
+reg_w = np.array(range(-2, 3))/4 + 11
 #reg_v = [0.01]
-reg_v = np.array([0.01, 0.1, 1])
-lrs = np.array([1e-3, 1e-4])
+reg_v = np.array(range(1, 11))/50
+lrs = np.array([1e-4])
 iters = range(1)
 
-os.system('touch out_mnist4.json')
+os.system('touch out_mnist5.json')
 
 
 
